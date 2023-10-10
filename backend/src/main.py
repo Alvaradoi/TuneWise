15d64ef42a7d1c03eef8a2bf424c2d01613459d9
Isaac Alvarado <isaacmcalvarado@gmail.com>
2023-10-09 23:58:26 -0700

    Added .env, modified main py to work

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,42 +1,26 @@
+# Import required modules
+from dotenv import load_dotenv
+import os
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
+# Load environment variables from .env file
+load_dotenv()
 
-def get_spotify_data():
-    # Set up credentials (Replace with your own client ID and client secret)
-    client_id = "ca1fc94bd5a44b2ba8e66367802a5285"
-    client_secret = "99b261787d1940aaa4915208e8cec5c1"
+# Retrieve environment variables
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
 
-    # Initialize Spotipy with user credentails
-    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+# Initialize Spotipy with user credentails
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope="user-library-read"))
 
-    # Search for a playlist (Replace with your desired search query)
-    results = sp.search(q='Your Search Query', limit=20, type='playlist')
-    playlists = results['playlists']['items']
+# Example: Show some of the user's playlists
+results = sp.current_user_playlists(limit=10)
+for idx, item in enumerate(results['items']):
+    print(f"{idx + 1}) {item['name']}")
 
-    # List out the playlists found
-    for idx, playlist in enumerate(playlists):
-        print(f"{idx + 1}. {playlist['name']}")
-
-
-def main():
-    print("Welcome to SpotiWise!")
-
-    # Initial menu for user to decide what they want to do
-    print("1. Search Spotify Playlists")
-    print("2. Analyze Playlist")
-    # Add more options as your project grows
-
-    choice = input("Please enter the number of your choice: ")
-
-    if choice == '1':
-        get_spotify_data()
-    elif choice == '2':
-        print("This feature is under development.")
-        # Replace with function that analyzes a playlist
-    else:
-        print("Invalid choice. Exiting.")
-
-
-if __name__ == "__main__":
-    main()
+#
